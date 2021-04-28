b5ff0ae9d94225cf696009ce20104de2d6fcbcd6
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-04-28 21:57:23 +0900

    add skeleton

--- chrome-extension/src/js/background.js
+++ chrome-extension/src/js/background.js
@@ -1,1 +1,7 @@
-console.log("background javascript running")
+chrome.extension.onConnect.addListener(function(port) {
+    console.log("Connected .....");
+    port.onMessage.addListener(function(msg) {
+        console.log("message recieved " + msg);
+        port.postMessage("Hi Popup.js");
+    });
+});
--- chrome-extension/src/js/popup.js
+++ chrome-extension/src/js/popup.js
@@ -1,11 +1,29 @@
-var EVENT_DOM_CONTENT_LOADED = 'DOMContentLoaded';
-
-function setEnviroment() {
-  console.log("setting enviroment")
-}
+let EVENT_DOM_CONTENT_LOADED = 'DOMContentLoaded';
 
 function fireWhenDOMContentIsLoaded() {
-  setEnviroment();
+  document.getElementById("goToTheDetail").onclick = () =>{
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+  }
+  
+  document.getElementById("saveStatusBtn").onclick = () => {
+    port.postMessage("Hi BackGround");
+  }
 }
+
+
+let port = chrome.extension.connect({
+  name: "Sample Communication"
+});
+
+port.onMessage.addListener(function(msg) {
+  console.log("message recieved" + msg);
+});
+
+
+
 
 document.addEventListener(EVENT_DOM_CONTENT_LOADED, fireWhenDOMContentIsLoaded);
