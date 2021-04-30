7593875bba8a0f1a60a5970896348143d8b6db64
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-04-30 20:21:07 +0900

    get url status

--- chrome-extension/src/js/background.js
+++ chrome-extension/src/js/background.js
@@ -1,7 +1,63 @@
-chrome.extension.onConnect.addListener(function(port) {
-    console.log("Connected .....");
-    port.onMessage.addListener(function(msg) {
-        console.log("message recieved " + msg);
-        port.postMessage("Hi Popup.js");
+const pluck = (arrayOfObject, property) => {
+    return arrayOfObject.map(function (item) {
+      return item[property];
     });
-});
+};
+
+async function getIds (windows) {
+    return pluck(windows,'id')
+}
+
+const getUrlStatus = async(ids) => {
+    let urlStatus = []
+    ids.forEach((id,index,array) => {
+        chrome.tabs.getAllInWindow(id,(tabs => {
+            urlStatus.push(pluck(tabs,'url'))
+        }))
+    })
+    return urlStatus 
+}
+
+
+const getCurrentStatus = async (windows)=>{
+    let ids = await getIds(windows)
+    let urlStatus = await getUrlStatus(ids)
+    return urlStatus
+}
+
+const resolveCurrentStatus = (o) => new Promise(resolve => {
+    chrome.windows.getAll(o, async (windows) => {
+        const currenStatus = await getCurrentStatus(windows)
+        resolve(currenStatus)
+    })    
+})
+
+// const getAllWindows = async () => {
+//     chrome.windows.getAll({populate: true},(data) => {return data})
+// }
+
+const save = () => new Promise(resolve => {
+    resolveCurrentStatus({populate:true})
+    .then(result => { // result: status
+        resolve(result)
+    })
+})
+
+const msgController = async (port) => {
+    console.assert(port.name == "messaging");
+    port.onMessage.addListener(async (msg) => {
+        if (msg.task === "save") {
+            save().then(urlStatus => {
+                console.log("urlStatus ",urlStatus)
+                // getting url status
+
+                port.postMessage({result:true,task:"saveResponse"});
+            }).catch(err => {
+                port.postMessage({result:false});
+            })
+            
+        }
+    })
+}
+
+chrome.runtime.onConnect.addListener(msgController);
--- chrome-extension/src/js/popup.js
+++ chrome-extension/src/js/popup.js
@@ -1,29 +1,56 @@
-let EVENT_DOM_CONTENT_LOADED = 'DOMContentLoaded';
-
-function fireWhenDOMContentIsLoaded() {
-  document.getElementById("goToTheDetail").onclick = () =>{
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-    } else {
-      window.open(chrome.runtime.getURL('options.html'));
-    }
-  }
-  
-  document.getElementById("saveStatusBtn").onclick = () => {
-    port.postMessage("Hi BackGround");
+const openOptionPage = () => {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
   }
 }
 
+const toggleBtn = (element) => {
+  element.disabled = !element.disabled
+}
 
-let port = chrome.extension.connect({
-  name: "Sample Communication"
+const disableBtn = (element) => {
+  element.disabled = true
+}
+
+const changeText = (element,text) => {
+  element.innerText = text
+}
+let port = chrome.runtime.connect({name: "messaging"});
+
+const saveStatusBtn = document.getElementById('saveStatusBtn');
+const goToTheDetailBtn = document.getElementById("goToTheDetailBtn")
+
+const saveTabs = () =>{
+  console.log("save tabs")
+  const saveStatusBtn = document.getElementById('saveStatusBtn');
+  disableBtn(saveStatusBtn)
+  changeText(saveStatusBtn,"saving..")
+  port.postMessage({task: "save"});
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  goToTheDetailBtn.addEventListener('click', openOptionPage);
+  saveStatusBtn.addEventListener('click', saveTabs)
 });
 
-port.onMessage.addListener(function(msg) {
-  console.log("message recieved" + msg);
+port.onMessage.addListener( async (response) => {
+  console.log("response : ",response);
+  if (response?.task == "saveResponse"){
+    if (response.result) {
+      changeText(saveStatusBtn,"complete!")
+    } else {
+      changeText(saveStatusBtn,"failed..")
+    }
+    setTimeout(() => {
+      changeText(saveStatusBtn,"Save status")
+      toggleBtn(saveStatusBtn)
+    },1000)
+  }
+
+  else if (response?.question == "Madame who?")
+    port.postMessage({answer: "Madame... Bovary"});
 });
-
-
-
-
-document.addEventListener(EVENT_DOM_CONTENT_LOADED, fireWhenDOMContentIsLoaded);
